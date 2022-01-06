36813276dbf35aae82d3be382c862d132d8c33dd
Miguel Armas <miguel.armas.abt@gmail.com>
2022-01-06 16:42:33 -0500

    unicersityCardList.xsd corregido

--- src/main/identification/utils/Constants.java
+++ src/main/identification/utils/Constants.java
@@ -3,7 +3,7 @@ package main.identification.utils;
 public class Constants {
 
   public static final String XML_DOCUMENT_LIST = "C:\\dev\\laboratorio5\\src\\resources\\universityCardList.xml";
-  public static final String XML_DOCUMENT = "src/resources/universityCard.xml";
-  public static final String XML_SCHEMA = "src/resources/universityCard.xsd";
+  public static final String XML_DOCUMENT = "src/resources/universityCardList.xml";
+  public static final String XML_SCHEMA = "src/resources/universityCardList.xsd";
   public static final String MESSAGE_XML_DOCUMENT = "Documento XML";
 }
